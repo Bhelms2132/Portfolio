3a4539ad2bcff8f6c49b6ae035a7f53b1ca0a244
Brian Helms <bhelms2132@gmail.com>
2022-07-12 18:28:09 -0400

    Functions added in javascript to open and close slide menu with some css added for syling

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Function opens slide menu
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width= '250px';
+    document.getElementById('container').style.marginLeft= '250px';
+}
+
+// Function closes slide menu
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width= '0';
+    document.getElementById('container').style.marginLeft= '0';
+}
